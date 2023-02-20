0f18a6d20526819e7fc0c081b2b97aa20f1fe1f1
damien911224 <damien911224@gmail.com>
2023-02-20 15:18:27 +0900

    Add files

--- src/main.py
+++ src/main.py
@@ -793,7 +793,7 @@ if __name__ == "__main__":
 
             # train
             "seed": 2023,
-            "epochs": 3000 if args.dataset == "thumos14" else 80,
+            "epochs": 3000 if args.dataset == "thumos14" else 120,
             "lr": 2.0e-4 if args.dataset == "thumos14" else 1.0e-4,
             "validation_term": 10 if args.dataset == "thumos14" else 10,
             "ckpt_save_term": 100 if args.dataset == "thumos14" else 5,
