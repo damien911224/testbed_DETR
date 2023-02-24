446c8b8803054587b2f6b446dd64b3be68246d66
damien911224 <damien911224@gmail.com>
2023-02-25 03:52:41 +0900

    Add files

--- src/main.py
+++ src/main.py
@@ -854,7 +854,7 @@ if __name__ == "__main__":
             "feature_frame_step_size": 8,
             "video_fps": 25.0,
             "temporal_width": 64,
-            "feature_width": 256 if args.dataset == "thumos14" else 256,
+            "feature_width": 256 if args.dataset == "thumos14" else 1024,
             "dformat": "NDHWC",
             "copypaste_prob": 0.00,
 
