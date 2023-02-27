aa84b47a48b58e19f3ca3b7e246304656057ef52
damien911224 <damien911224@gmail.com>
2023-02-27 23:04:58 +0900

    Add files

--- src/models/dab_transformer.py
+++ src/models/dab_transformer.py
@@ -319,14 +319,14 @@ class TransformerEncoderLayer(nn.Module):
                 src_mask: Optional[Tensor] = None,
                 src_key_padding_mask: Optional[Tensor] = None,
                 pos: Optional[Tensor] = None):
-        # q = k = self.with_pos_embed(src, pos)
-        # src2, K_weights = self.self_attn(q, k, value=src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
+        q = k = self.with_pos_embed(src, pos)
+        src2, K_weights = self.self_attn(q, k, value=src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
 
-        q = k = src.transpose(1, 0)
-        # q = k = self.with_pos_embed(src, pos).transpose(1, 0)
-        src2, K_weights = self.self_attn(q, k, value=src.transpose(1, 0),
-                                         attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
-        src2 = src2.transpose(1, 0)
+        # q = k = src.transpose(1, 0)
+        # # q = k = self.with_pos_embed(src, pos).transpose(1, 0)
+        # src2, K_weights = self.self_attn(q, k, value=src.transpose(1, 0),
+        #                                  attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
+        # src2 = src2.transpose(1, 0)
 
         # print(torch.argsort(-K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
         # print(torch.max(K_weights[0].detach().cpu(), dim=-1)[0][:10])
